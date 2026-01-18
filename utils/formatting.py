52d63cbe223787c4a293c5ba65d776c6b50c1f75
Redox <69946827+wrefgtzweve@users.noreply.github.com>
2026-01-18 03:04:05 +0100

    Support negatives in `format_size`

--- utils/formatting.py
+++ utils/formatting.py
@@ -1,13 +1,17 @@
 def format_size(size: int | float) -> str:
+    is_negative = size < 0
+    size = abs(size)
+    
     if size < 1000:
-        return f"{int(size)} B"
-
-    if size < 1_000_000:
+        result = f"{int(size)} B"
+    elif size < 1_000_000:
         kbs = size / 1000
-        return f"{round(kbs, 2)} KB"
-
-    mbs = size / 1_000_000
-    return f"{round(mbs, 2)} MB"
+        result = f"{round(kbs, 2)} KB"
+    else:
+        mbs = size / 1_000_000
+        result = f"{round(mbs, 2)} MB"
+    
+    return f"-{result}" if is_negative else result
 
 def format_percentage(part: int | float, whole: int | float) -> str:
     if whole == 0:
